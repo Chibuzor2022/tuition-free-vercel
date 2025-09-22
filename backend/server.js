a354c3cd38b1b0d4ae89923200e371318eb45e35
Chibuzor2022 <incovenant@hotmail.com>
2025-09-22 11:44:40 +0100

    dotenv

--- backend/server.js
+++ backend/server.js
@@ -59,13 +59,9 @@ app.use(errorHandler);
 
 
 // Start server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server running on port ${PORT}`);
-// });
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
+
 
