16a9af6442a033b6c8e390d7e63c9a133429766a
Chibuzor2022 <incovenant@hotmail.com>
2025-09-22 14:47:21 +0100

    complete2

--- backend/server.js
+++ backend/server.js
@@ -65,7 +65,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   // Catch-all for React Router (must come after API routes & static files)
-  app.get("*", (req, res) =>
+  app.get(/.*/, (req, res) =>
     res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"))
   );
 }
