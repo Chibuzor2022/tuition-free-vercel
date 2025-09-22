3a0e7645926d159e3c78883880b09c0686a39a5a
Chibuzor2022 <incovenant@hotmail.com>
2025-09-22 14:17:39 +0100

    corsorigin

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,16 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser())
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use(cors([
+   "https://your-frontend.onrender.com",
+   "http://localhost:5173"
+
+]
+)) 
+ 
+
+
+
 
 
 // API routes
